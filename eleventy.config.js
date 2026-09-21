87ca68008fdf4962c1a2a48d53d6ae711714178f
stedi <teuvo@teuvovaisanen.fi>
2026-09-21 13:07:19 +0300

    Alkuasetukset

--- eleventy.config.js
+++ eleventy.config.js
@@ -75,12 +75,12 @@ export default async function(eleventyConfig) {
 			limit: 10,
 		},
 		metadata: {
-			language: "en",
-			title: "Blog Title",
-			subtitle: "This is a longer description about your blog.",
-			base: "https://example.com/",
+			language: "fi",
+			title: "Stedi's Eleventy Base Blog v9",
+			subtitle: "Kirjoittelen mitä sattuu.",
+			base: "https://stedi.rocks/",
 			author: {
-				name: "Your Name"
+				name: "Teuvo Väisänen"
 			}
 		}
 	});
